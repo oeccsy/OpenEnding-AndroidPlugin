b1097a1d74cf0f7ffeb353a447ca57d6a612a1c8
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-08-04 05:28:30 +0900

    feat : Service, Characteristic 생성

---
+++ OpenEndingAndroidPlugin/openending-ble/src/main/java/com/oeccsy/openending_ble/GameProfile.java
@@ -0,0 +1,43 @@
+package com.oeccsy.openending_ble;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+
+import java.util.UUID;
+
+public class GameProfile {
+    public static UUID GAME_SERVICE = UUID.fromString("00001805-0000-1000-8000-00805f9b34fb");
+    public static UUID TEST_A = UUID.fromString("00002a2b-0000-1000-8000-00805f9b34fb");
+    public static UUID TEST_B = UUID.fromString("00002a0f-0000-1000-8000-00805f9b34fb");
+
+    public static UUID CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
+    private static BluetoothGattService ownService;
+
+    public static BluetoothGattService getService() {
+        if(ownService == null) return createService();
+
+        return ownService;
+    }
+
+    public static BluetoothGattService createService()
+    {
+        ownService = new BluetoothGattService(GAME_SERVICE, BluetoothGattService.SERVICE_TYPE_PRIMARY);
+
+        BluetoothGattCharacteristic testA = new BluetoothGattCharacteristic(
+                TEST_A,
+                BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE | BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+                BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
+
+        BluetoothGattDescriptor configDescriptor = new BluetoothGattDescriptor(
+                CLIENT_CHARACTERISTIC_CONFIG,
+                BluetoothGattDescriptor.PERMISSION_READ | BluetoothGattDescriptor.PERMISSION_WRITE);
+
+        testA.addDescriptor(configDescriptor);
+
+        ownService.addCharacteristic(testA);
+
+        return ownService;
+    }
+}
